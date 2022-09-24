e6834da40a5640a3d37400855ef463b5b791ac93
rammsanker <109334253+rammsanker54@users.noreply.github.com>
2022-09-24 11:53:45 +0530

    Add files via upload

---
+++ marriage eliiblity/new  2.js
@@ -0,0 +1,32 @@
+
+
+
+
+
+
+var a = 21;
+var gender = "male";
+if ((a>=21)&&(gender=="male"))
+{console.log("fit for marriage");
+ elseif ((gender=="female") && (age>19))
+{console.log("fit for marriage");
+else
+{console.log("unfit for marriage")}
+
+var age = 61;
+if ((age>=21)&&(age<50))
+{console.log("allowed")}
+else if ((age<21){
+    console.log(" if parrent permitted");
+}
+else {
+    console.log("not permitted");
+}
+var a = 20;
+var gender = "female";
+if ((a>=21)&&(gender=="male"))
+{console.log("fit for marriage");}
+ else if ((a>19) && (gender=="female"))
+{console.log("fit for marriage");}
+else {
+console.log("unfit for marriage");}
